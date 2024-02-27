2f88959104c81f571b6da719c6085a2c4483e336
cyrusc2026 <cyrusc2026@student.cis.edu.hk>
2024-02-27 10:38:20 +0800

    Test 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 // I have neither given nor received unauthorized aid on this piece of work.
 
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,19 +8,51 @@ public class Main {
     }
 
     // 1. add
-    
-    // 2. add
+    public static int add(int a, int b) {
+    int sum = (a+b);
+        return sum;
+    }
 
+    // 2. add
+public static int add(int a,int b, int c, int d){
+        int twonumbers = add(a,b);
+        int threenumbers = add(twonumbers,c);
+        int fournumbers = add(threenumbers,d);
+        return fournumbers;
+}
     // 3. morningGreeting
+    public static String morningGreeting(String name) {
+        String morningGreeting = ("早上好,  "+ name + "!");
+        return morningGreeting;
+    }
 
     // 4. afternoonGreeting
-
-    // 5. triple
-
+    public static String afternoonGreeting(String name) {
+        String afternoonGreeting = ("下午好,  " + name + "!");
+        return afternoonGreeting;
+    }
+// 5. triple
+        public static String triple(String name) {
+            String triple = (name + name + name);
+            return triple;
+        }
     // 6. half
+    public static double half(int number) {
+        double half = (number/ 2);
+        return half;
+    }
 
     // 7. roundPositiveValueToNearestInteger
+    public static int roundPositiveValueToNearestInteger(double number) {
+        int roundednumber = (int) Math.round (number);
+        return roundednumber;
+    }
+
 
     // 8. roundNegativeValueToNearestInteger
+    public static int roundNegativeValueToNearestInteger(double number) {
+        int roundednumber = (int) Math.round (number);
+        return roundednumber;
+    }
 
 }
